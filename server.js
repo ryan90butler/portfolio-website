c7aabb5ec9d3a6449b07cc823e1c7e193fdf9d9a
Ryan Butler <b6utle5r@gmail.com>
2018-07-27 11:04:04 -0600

    test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, '/build')));
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
